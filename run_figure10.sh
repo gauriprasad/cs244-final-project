7b92667173a1c0cf46ded1eac1640c012f097fca
Gauri Prasad <gauriprasad@Gauris-MacBook-Pro.local>
2021-05-13 17:58:31 -0700

    Added code for figure10

---
+++ run_figure10.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for DELAY in "1ms" "2.5ms" "5ms" "10ms" "25ms" "50ms" "100ms" "250ms"; do
+  for PROTOCOL in "TcpNewReno" "TcpVegas" "TcpWestwood" "TcpWestwoodPlus"; do
+    DIR=outputs/$PROTOCOL
+    [ ! -d $DIR ] && mkdir $DIR
+
+    # Run the NS-3 Simulation
+    ./waf --run "figure10 --transport_prot=$PROTOCOL --delay_wired=$DELAY"
+  done
+done
